5f6b9fe86f14f408bad8099d367ab9756174aa9f
Blagica Bozhinovska <b.bozhinovska@reukema.com>
2023-12-01 20:22:59 +0100

    Working second part

--- first/first.js
+++ first/first.js
@@ -13,6 +13,15 @@ const letters = {
     "seven": 7,
     "eight": 8,
     "nine": 9,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
 };
 let sum = 0;
 
@@ -23,32 +32,53 @@ var lineReader = readline.createInterface({
 
 lineReader.on("line", function (line) {
     // first replace all written numbers with digits, minding the order
-    console.log(line);
-    const split = line.split('');
-    let newString = '';
-    split.map((char, index) => {
-        newString += char;
-        if (index > 2) {
-            Object.keys(letters).forEach((key) => {
-                if (newString.includes(key)) {
-                    line = line.replaceAll(key, letters[key]);
-                    newString = '';
-                }
-            });
+    let string = '';
+    let found = false;
+    for (let j = 0; j < line.length; j++) {
+        if (found) {
+            found = false;
+            break;
         }
-    });
+        const char = line[j];
+        string += char;
+        for (let i = 0; i < Object.keys(letters).length; i++) {
+            const key = Object.keys(letters)[i];
+            let indexOf = string.indexOf(key);
+            if (indexOf >= 0) {
+                line = line.replace(key, letters[key]);
+                found = true;
+                break;
+            }
+        };
+    }
 
-    console.log(line);
+    string = '';
+    found = false;
+    const reversed = line.split('').reverse();
+    for (let j = 0; j < reversed.length; j++) {
+        if (found) {
+            break;
+        }
+        const char = reversed[j];
+        string = char + string;
+        console.log('string', string);
+        for (let i = 0; i < Object.keys(letters).length; i++) {
+            const key = Object.keys(letters)[i];
+            if (string.indexOf(key) >= 0) {
+                line = line.replaceAll(key, letters[key]);
+                found = true;
+                break;
+            }
+        };
+    }
+
     // then get the numbers
     const number = line.replace(/\D/g, "");
-    console.log(number);
     // then get the first and last digit
     const toString = number.toString();
     const firstAndLast = toString[0] + toString[toString.length - 1];
 
-    console.log(firstAndLast);
     sum += parseInt(firstAndLast, 10);
-    console.log('---');
 });
 
 lineReader.on("close", function () {
